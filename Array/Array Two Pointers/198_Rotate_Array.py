00fb7ee6878ba1d99d63229ca421f6d8e9ee7f6f
arif599 <arifullashaik01@gmail.com>
2022-01-24 09:33:44 -0800

    adding optimal solution

--- Array/Array Two Pointers/198_Rotate_Array.py
+++ Array/Array Two Pointers/198_Rotate_Array.py
@@ -1,3 +1,37 @@
+def rotateSolution2(nums, k):
+    """
+    Optimal Approach: 
+        - Trying to optimize the space complexity by doing it in place
+        - Reverse the array using two pointer apprach
+        - Then reverse the array form 0 to k-1 
+        - Then reverse the array from K to n part of the array
+
+    Analysis:
+        - Time: O(n) becasue we have to iterate over every element in the array
+        - Space: O(1) because we are doing it in place
+    """
+    # Base cases
+    if len(nums) <= 1:
+        return nums
+    if k > len(nums):
+        k = k%len(nums) # if k is greater than the length of the array then we need to mod k by the length of the array
+    
+    # reverse the entire array in place
+    reserveList(nums, left = 0, right = len(nums)-1) 
+    # reverse the array from 0 to k-1
+    reserveList(nums, left = 0, right = k-1)
+    # reverse the array from K to n
+    reserveList(nums, left = k, right = len(nums)-1)
+
+    return nums
+
+def reversePartofList(nums, left, right):
+    while left < right:
+        # swap the elements
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right += 1
+
 def rotate(nums, k):
     """
     Initial Approach: 
