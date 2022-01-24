1b08519feba688f0d1efe8064795c954d363e4fd
arif599 <arifullashaik01@gmail.com>
2022-01-24 10:18:17 -0800

    adding another solution

--- Array/Array Two Pointers/977_Squares_of_a_Sorted_Array.py
+++ Array/Array Two Pointers/977_Squares_of_a_Sorted_Array.py
@@ -1,4 +1,15 @@
 def sortedSquares(nums):
+    """
+    Initial Approach: (final solution)
+        - Iterate over the part of the array that needs to be rotated (n-k to n) and add it to result array
+        - Iterate over the rest of the array (0 to k) and add it to result array
+        - Return result array
+
+    Analysis:
+        - Time: O(n) becasue we have to iterate over every element in the array
+        - Space: O(n) because we have to create a new array (result) of the same size
+    """
+    
     # Two pointers
     left = 0
     right = len(nums)-1
@@ -19,3 +30,42 @@ def sortedSquares(nums):
             right -= 1
         
     return result
+
+
+
+# does not work for all negative numbers in a list
+def sortedSquaresSolution2(nums):
+    """
+    Initial Approach: (failed solution)
+        - Create a left=0 and right=n-1 pointer
+        - Compare the values of the left and right pointers
+        - Only move the right pointer, all the elements to the right of the right pointer are squared
+
+    Analysis:
+        - Time: O(n) becasue we have to iterate over every element in the array
+        - Space: O(1) because we are doing it in place
+    """
+    # initilize the 2 pointers
+    left = 0
+    right = len(nums)-1
+    
+    while left < right:
+        # compare the elements
+        if nums[left]**2 > nums[right]**2:
+            swapSquared(nums, left, right)
+        elif nums[left]**2 == nums[right]**2:
+            squareRight(nums, right)
+            swapSquared(nums, left, right)
+        else:
+            squareRight(nums, right)
+            squareRight(nums, right)
+            
+    return nums
+
+def squareRight(nums, right):
+    nums[right] = nums[right]**2
+    right -= 1
+def swapSquared(nums, left, right):
+    nums[left], nums[right] = nums[right], nums[left]
+    nums[right] = nums[right]**2
+    right -= 1
